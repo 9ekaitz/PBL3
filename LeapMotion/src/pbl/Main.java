4105d966eb98dba9f14126639413c230315b4588
9ekaitz <ekaitz.casado@alumni.mondragon.edu>
2020-12-14 10:09:00 +0100

    no changes

--- LeapMotion/src/pbl/Main.java
+++ LeapMotion/src/pbl/Main.java
@@ -1,4 +1,5 @@
 package pbl;
+
 //master2
 import java.io.IOException;
 
@@ -11,34 +12,32 @@ public class Main {
 
 	public Main() {
 		SampleListener listener = new SampleListener();
-        Controller controller = new Controller();
-        
-        Frame frame = controller.frame(); // controller is a Controller object
-        HandList hands = frame.hands();
-        Hand firstHand = hands.get(0);
-        
-                
-        // Have the sample listener receive events from the controller
-        controller.addListener(listener);
+		Controller controller = new Controller();
 
-        // Keep this process running until Enter is pressed
-        System.out.println("Press Enter to quit...");
-        try {
-            System.in.read();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+		Frame frame = controller.frame(); // controller is a Controller object
+		HandList hands = frame.hands();
+		Hand firstHand = hands.get(0);
 
-        // Remove the sample listener when done
-        
-        controller.removeListener(listener);
-        
-System.out.println("despues");
-    }
+		// Have the sample listener receive events from the controller
+		controller.addListener(listener);
 
-	
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        Main p = new Main();
-    }
+		// Keep this process running until Enter is pressed
+		System.out.println("Press Enter to quit...");
+		try {
+			System.in.read();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		// Remove the sample listener when done
+
+		controller.removeListener(listener);
+		
+		System.out.println("despues");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Main p = new Main();
+	}
 }
