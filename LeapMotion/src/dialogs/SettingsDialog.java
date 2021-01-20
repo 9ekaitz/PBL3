a13600fe8d689c8c544e1f58f5dc7a78b15a4ee5
9ekaitz <ekaitz.casado@alumni.mondragon.edu>
2021-01-20 08:33:03 +0100

    Portuen dialogoa sortuta

--- LeapMotion/src/dialogs/SettingsDialog.java
+++ LeapMotion/src/dialogs/SettingsDialog.java
@@ -2,9 +2,11 @@ package dialogs;
 
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.Insets;
 
 import javax.swing.BorderFactory;
@@ -15,6 +17,8 @@ import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 import models.PortManager;
 
@@ -22,6 +26,7 @@ public class SettingsDialog extends JDialog {
 
 	JFrame frame;
 	JComboBox<String> ports;
+	JTextField l;
 	PortManager portManager;
 	
 	public SettingsDialog(JFrame frame, String title, boolean mode) {
@@ -114,13 +119,15 @@ public class SettingsDialog extends JDialog {
 		panel.add(label, labelKonst);
 		
 		/* JComboBox bat portu ezberdinak erakusteko, eta boti bat portuen zerrenda freskatzeko */
-		JPanel subPanel = new JPanel();
+		JPanel subPanel = new JPanel(new GridLayout(2, 1, 0, 5));
+		
+		JPanel azpiPanel = new JPanel();
 		ports = new JComboBox<String>(portManager.getAvaiblePorts());
-		ports.setForeground(Color.WHITE);
-		ports.setBackground(new Color(36, 123, 160));
+		ports.setForeground(Color.BLACK);
+//		ports.setBackground(Color.LIGHT_GRAY);
 		ports.setActionCommand("logout");
 //		btnLogout.addActionListener(controller);
-		subPanel.add(ports);
+		azpiPanel.add(ports);
 		
 		JButton button = new JButton(new ImageIcon("res/icons/refresh.png"));
 		button.setActionCommand("refresh");
@@ -129,7 +136,26 @@ public class SettingsDialog extends JDialog {
 		button.setBorder(BorderFactory.createEmptyBorder(5,5,5,7));
 		button.setOpaque(false);
 		button.setFocusPainted(false);
-		subPanel.add(button);
+		azpiPanel.add(button);
+		
+		subPanel.add(azpiPanel);
+		
+		azpiPanel = new JPanel();
+		
+		l = new JTextField();
+		l.setPreferredSize(new Dimension(200, 25));
+		l.setText("Status");
+		l.setEditable(false);
+		l.setBorder(BorderFactory.createLineBorder(Color.GRAY));
+		azpiPanel.add(l);
+		
+		button = new JButton("Open");
+		button.setForeground(Color.WHITE);
+		button.setBackground(new Color(36, 123, 160));
+		button.setActionCommand("open");
+		azpiPanel.add(button);
+		
+		subPanel.add(azpiPanel);
 		
 		GridBagConstraints subPanelKonst = new GridBagConstraints();
 		subPanelKonst.insets = new Insets(0, 0, 5, 0);
