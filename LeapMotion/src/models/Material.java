dbab881596f090e062470156ed913ee9f5127d26
9ekaitz <ekaitz.casado@alumni.mondragon.edu>
2021-01-20 15:16:32 +0100

    bbbb

--- LeapMotion/src/models/Material.java
+++ LeapMotion/src/models/Material.java
@@ -14,5 +14,13 @@ public class Material {
 	public String toString() {
 		return name;
 	}
+
+	public boolean isSelected() {
+		return selected;
+	}
+
+	public void setSelected(boolean selected) {
+		this.selected = selected;
+	}
 	
 }
